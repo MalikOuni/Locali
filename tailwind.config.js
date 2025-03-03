e5fdf715218eadf5b8c573d4cea6d01034499e7f
MaherOuni <maher.ouni21@gmail.com>
2025-03-03 01:03:30 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*", "./public/*"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-teal": "#C2DEDC",
+      },
+    },
+  },
+  plugins: [],
+};
